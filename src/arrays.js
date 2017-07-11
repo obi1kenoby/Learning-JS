1d4446fac3dadb1cac7e541068a966aa5fea509c
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-11 21:15:19 +0300

    simple array example...

---
+++ src/arrays.js
@@ -0,0 +1,12 @@
+var array = [1,2,3,4,5,6,7,8,9,0];
+
+document.write(array);
+
+document.write("<hr />");
+
+var year = ["January", "February", "March", "April", "May", "June", "July"," August", 
+			"September", "October", "November", "December"];
+
+for(var i=0; i<year.length; i++){
+	document.write(year[i] + "<br>");
+}
