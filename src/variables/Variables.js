23aba39796e62202544a545dfb6ae6b775ae6f85
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-12 21:43:19 +0300

    learn variables types

---
+++ src/variables/Variables.js
@@ -0,0 +1,25 @@
+//simple variables;
+
+var number = 2017, // numeric
+	string = "Hello World!", //string
+	bool = true, // boolean
+	myNull = null, // nullable
+	undef = undefined; // undefined
+
+console.log(number);
+console.log(string);
+console.log(bool);
+console.log(myNull);
+console.log(undef);
+
+//diferent variables;
+
+var alex = {name:"Alex", age:28}; // object
+	array = [1,2,3,4,5,6,7];	// array
+	fun = function(){};	// function
+	regexp = /w+/g; //	regular expression
+
+console.log(typeof alex); //type of object or variable
+console.log(typeof array);
+console.log(typeof fun);
+console.log(typeof regexp);
