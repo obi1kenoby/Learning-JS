a963b0882f588677cdd8c69107d619ce1c67a648
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-13 21:40:30 +0300

    learn simple string class

---
+++ src/strings/MyString.js
@@ -0,0 +1,13 @@
+console.log("Hello World");		// Hello World
+console.log('Hello World!');	// Hello World!
+
+var string = "Hello World";
+
+console.log(string.charAt(0));	// return character of 0 index
+console.log(string.substring(3));	// return substring from 3 to lenth of string
+
+console.log(string.indexOf("ll")); // return index of substring
+console.log(string.replace("ll", "nn")); //replace substring
+console.log(string.split(" "));	//split string by words
+console.log(string[7]);	//return character of index 7 like in array
+console.log(string.toUpperCase()); //convert string to upper case
