10e0f82cdb7919fc6f7aa52f32ad63d2c4776c4e
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-16 22:31:10 +0300

    add casting of types and conditional-expressions

---
+++ src/сonditional-expressions/conexpressions.js
@@ -0,0 +1,23 @@
+//сonditional operators work like Java or C++
+
+var a = 5;
+
+if (a > 3) console.log("3 realy less then a");
+
+if (a > 10) {
+	console.log("a more then 10");
+}
+else{
+	console.log("a less then 10");
+}
+
+a = 50;
+
+switch(a){
+	case 10: console.log(a); break;
+	case 20: console.log(a); break;
+	case 30: console.log(a); break;
+	case 60: console.log(a); break;
+	case 70: console.log(a); break;
+	default: console.log("no matches!!!");
+}
