d26167d78962e1f0c6b7f9cd42f366dcee95023b
Alexandr Naumov <ultras.1988@mail.ru>
2017-07-19 23:45:30 +0300

    learn cycles like in java

---
+++ src/Cycles.js
@@ -0,0 +1,22 @@
+//	cycle for...
+//	for(init; condition; incrеment;)
+
+var i;
+for(i = 0; i<10; i++){
+	console.log(i);
+}
+
+// cycle while..
+//while(condition)
+
+while(i<5){
+	console.log(i++);
+}
+
+// cycle do/while
+//	do instruction while(condition)
+
+do{
+	console.log(i++);
+}
+while(i>10);
